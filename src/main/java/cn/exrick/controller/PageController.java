82a311832087dc04082ebc2b61df4b3b24d4286b
gehaudong <503814588@qq.com>
2018-05-21 14:50:59 +0800

    update 2018-05-21

--- src/main/java/cn/exrick/controller/PageController.java
+++ src/main/java/cn/exrick/controller/PageController.java
@@ -1,16 +1,10 @@
 package cn.exrick.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -23,13 +17,11 @@ public class PageController {
 
     @RequestMapping("/")
     public String index(){
-
         return "index";
     }
 
     @RequestMapping("/{page}")
     public String showPage(@PathVariable String page){
-
         return page;
     }
 }
