4e2b09cb7b3c3dd0c4033352488a42fcf11c9b85
gehaudong <503814588@qq.com>
2018-05-22 14:03:54 +0800

    update 2018-05-22

--- src/main/java/cn/exrick/config/GeetestConfig.java
+++ src/main/java/cn/exrick/config/GeetestConfig.java
@@ -6,9 +6,9 @@ package cn.exrick.config;
 public class GeetestConfig {
 
 	// 填入自己的captcha_id和private_key
-    private static final String geetest_id = "b46d1900d0a894591916ea94ea91bd2c";
+    private static final String geetest_id = "e2febabf33c14d81bc704eb02dba6cf4";
 
-	private static final String geetest_key = "36fc3fe98530eea08dfc6ce76e3d24c4";
+	private static final String geetest_key = "99571dc57c2caf1a035fdf1c6f92b475";
 
 	private static final boolean newfailback = true;
 
